68a4c3c9b7313cb4ea4070ec7fd6e687b4707c8f
Pedro Martins Dietrich <pedro.martins.dietrich@gmail.com>
2023-10-22 20:49:12 -0300

    Creates and manages a GLFW window

---
+++ src/Window/Window.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Creates and handles the application window
+class Window
+{
+    public:
+        // Deletes the copy constructor
+        Window(const Window& win) = delete;
+
+        // Gets the only window instance (singleton)
+        static Window* getWindowInstance();
+
+        // Decides whether the window should close or not
+        bool manageWindow();
+        // Swaps buffers and handle window events
+        void updateWindow();
+
+    private:
+        // Window class single instance
+        static Window* windowInstance;
+
+        // GLFW's window object
+        GLFWwindow* glfwWindow;
+
+        // Private constructor
+        Window();
+
+        // Checks if the window was successfully created
+        void checkWindowCreation();
+        // Destroys window and and terminates GLFW
+        void terminate();
+};
